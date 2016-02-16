f954505833b85543b5ec70643314b5bb34f8a3e4
mattinmir <mattinmir@users.noreply.github.com>
2016-02-16 17:56:58 +0000

    Update initio.c
    need to do apx520 analysis

--- initio.c
+++ initio.c
@@ -31,20 +31,14 @@
 #include "dsk6713.h"
 #include "dsk6713_aic23.h"
 
-// math library (trig functions)
-#include <math.h>
-
 // Some functions to help with writing/reading the audio ports when using interrupts.
 #include <helper_functions_ISR.h>
 
-// PI defined here for use in your code 
-#define PI 3.141592653589793
+// Filter Coefficients stored in array 'double b[] = {......}'
+#include "fir_coef.txt"
 
-// Singegen lookup table size
-#define SINE_TABLE_SIZE 256 
-
-// Size of delay buffer
-#define BUFSIZE 12
+// Size of delay buffer = size of coefs array = total bytes/bytes of 1 element
+#define BUFSIZE sizeof(b)/sizeof(b[0])
 /******************************* Global declarations ********************************/
 
 /* Audio port configuration settings: these values set registers in the AIC23 audio 
@@ -69,70 +63,34 @@ DSK6713_AIC23_Config Config = { \
 
 // Codec handle:- a variable used to identify audio interface  
 DSK6713_AIC23_CodecHandle H_Codec;
-/* Sampling frequency in HZ. Must only be set to 8000, 16000, 24000
-32000, 44100 (CD standard), 48000 or 96000  */ 
-int sampling_freq = 8000;
 
-//Iterator used to iterate through sine table
-float iterator = 0;
 
-// Holds the value of the current sample 
-short sample = 0; 
-
-/* Left and right audio channel gain values, calculated to be less than signed 32 bit
- maximum value. */
-Int32 L_Gain = 2100000000;
-Int32 R_Gain = 2100000000;
-
-// Sinegen lookup table
-float table[SINE_TABLE_SIZE];
-
-/* Use this variable in your code to set the frequency of your sine wave 
-   be carefull that you do not set it above the current nyquist frequency! */
-float sine_freq = 1000.0; 
-
+// Delay buffer
 double x[BUFSIZE];
- /******************************* Function prototypes ********************************/
+
+// Output of filter
+double y;
+
+// Input read from signal generator
+short sample;
+/******************************* Function prototypes ********************************/
 void init_hardware(void);     
 void init_HWI(void);          
 void ISR_AIC(void);        
-float sinegen(void);
-void sine_init(void);
+void non_circ_FIR(void);
+void circ_FIR(void);
+
 /********************************** Main routine ************************************/
 void main(){      
 
- 
-	// initialize board and the audio port
+// initialize board and the audio port
   init_hardware();
 	
   /* initialize hardware interrupts */
-  init_HWI();
-  
-  sine_init();
-  	 		
-  /* loop indefinitely, waiting for interrupts */  					
-  while(1) 
- 	{
- 		// Calculate next sample
- 		//sample = sinegen();
- 		
-     	/* Send a sample to the audio port if it is ready to transmit.
-           Note: DSK6713_AIC23_write() returns false if the port if is not ready */
+  init_HWI();  	 		
 
-      /*  //  send to LEFT channel (poll until ready)
-        while (!DSK6713_AIC23_write(H_Codec, ((Int32)(sample * L_Gain))))
-        {}; 
-        
-		// send same sample to RIGHT channel (poll until ready)
-        while (!DSK6713_AIC23_write(H_Codec, ((Int32)(sample * R_Gain))))
-        {};
-        */
-        
-        
-		// Set the sampling frequency. This function updates the frequency only if it 
-		// has changed. Frequency set must be one of the supported sampling freq.
-		//set_samp_freq(&sampling_freq, Config, &H_Codec);	
-	}
+  /* loop indefinitely, waiting for interrupts */  					
+  while(1){}
   
 }
         
@@ -165,76 +123,42 @@ void init_HWI(void)
 {
 	IRQ_globalDisable();			// Globally disables interrupts
 	IRQ_nmiEnable();				// Enables the NMI interrupt (used by the debugger)
-	IRQ_map(IRQ_EVT_XINT1,4);		// Maps an event to a physical interrupt
-	IRQ_enable(IRQ_EVT_XINT1);		// Enables the event
+	IRQ_map(IRQ_EVT_RINT1,4);		// Maps an event to a physical interrupt
+	IRQ_enable(IRQ_EVT_RINT1);		// Enables the event
 	IRQ_globalEnable();				// Globally enables interrupts
 } 
 
 /******************** WRITE YOUR INTERRUPT SERVICE ROUTINE HERE***********************/  
 
 /*
- * Rectifies and outputs the sinewave
+ * Convolves input with filter and outputs
  */
  
 void ISR_AIC(void)
 {	
+	sample = mono_read_16Bit();
 	
-/*
- * Since we have to write a 16 bit value, we cast to `short`, since there is no half 
- * precision floating point number type in C. However, since our sample will be between 
- * -1 and 1, casting to a short would mean we would only ever get the values 0, 1 and
- * -1. So, we scale the value up by  2^15-1 (since 2^15 would cause overflow and 
- * incorrect output) before casting to ensure we don't lose information in the cast.
-*/
+	non_circ_FIR();
+	 
+	mono_write_16Bit((short)y); 
+}
 
+void non_circ_FIR(void)
+{
 	int i;
 	for (i = BUFSIZE; i > 0; i--)
 		x[i] = x[i-1];
-	x[0] = mono_read_16Bit();
-		
-	mono_write_16Bit(sample);
+	x[0] = sample;
 	
-	
-}
-/********************************** sinegen() ***************************************/   
-
-/* 
- * Controls output frequency by controlling rate at which samples are chosen from 
- * lookup table.
- * Iterating through the lookup table with larger step size forms higher frequency
- * output and vice versa. 
- */
- 
-float sinegen(void)
-{
-	
-	float step_size;
-	step_size = (sine_freq/(float)sampling_freq)*SINE_TABLE_SIZE;
-	
-	iterator += step_size;
-	
-	// Wrapping around the sine table when the iterator exceeds its bounds
-    if(iterator >= (SINE_TABLE_SIZE))
-    {
-       	iterator -= SINE_TABLE_SIZE;   
-    }
-	
-	// Typecast to int as array index needs to be integer type
-	return table[(int)iterator];  
-} 
-
-/********************************** sine_init() ***************************************/ 
-
-/* 
- * Initialises the lookup table with samples of a sine wave
- */
- 
-void sine_init(void)
-{
-	int i;
-	for(i=0; i < SINE_TABLE_SIZE; i++)
+	y=0;
+	for (i = BUFSIZE; i > 0; i--)
 	{
-		table[i] = sin(((float)i/SINE_TABLE_SIZE)*2*PI);
+		y += (x[i] * b[i]);
 	}
+	
 }
-  
+
+void circ_FIR(void)
+{
+	
+}
